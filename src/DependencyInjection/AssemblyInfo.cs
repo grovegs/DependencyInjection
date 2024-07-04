7eca3b98332e5590b1f37ae9faa00c911929e32e
Onur Tanrıkulu <onurtnrkl@gmail.com>
2024-07-04 20:41:07 +0300

    feat: update godot project

--- src/DependencyInjection/AssemblyInfo.cs
+++ src/DependencyInjection/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("DependencyInjection.Godot")]
 [assembly: InternalsVisibleTo("DependencyInjection.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
