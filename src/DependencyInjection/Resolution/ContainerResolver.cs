d46201700ba48b681aa3152c50425dea65dc0172
Onur Tanrıkulu <onurtnrkl@gmail.com>
2024-09-10 17:15:55 +0300

    fix: remove nullables

--- src/DependencyInjection/Resolution/ContainerResolver.cs
+++ src/DependencyInjection/Resolution/ContainerResolver.cs
@@ -2,23 +2,23 @@
 
 internal sealed class ContainerResolver : IContainerResolver
 {
-    private readonly IDictionary<Type, IInstanceResolver> _instanceResolversByRegistrationTypes;
-    private readonly IRegistrationResolver? _parent;
+    private readonly Dictionary<Type, IInstanceResolver> _instanceResolversByRegistrationTypes;
+    private readonly IRegistrationResolver _parent;
 
-    public ContainerResolver(IRegistrationResolver? parent)
+    public ContainerResolver(IRegistrationResolver parent)
     {
-        _instanceResolversByRegistrationTypes = new Dictionary<Type, IInstanceResolver>();
+        _instanceResolversByRegistrationTypes = [];
         _parent = parent;
     }
 
-    public object? Resolve(Type registrationType)
+    public object Resolve(Type registrationType)
     {
         if (_instanceResolversByRegistrationTypes.TryGetValue(registrationType, out var instanceResolver))
         {
             return instanceResolver.Resolve();
         }
 
-        return _parent?.Resolve(registrationType);
+        return _parent.Resolve(registrationType);
     }
 
     public void AddInstanceResolver(Type registrationType, IInstanceResolver instanceResolver)
